dc6af94c34ef806504ba7673174e449cebd94fb0
rahim çubuk <rahimcubuk@gmail.com>
2021-03-09 20:18:57 +0300

    16.Gun Ders Sonu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NavigatorComponent } from './components/navigator/navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
